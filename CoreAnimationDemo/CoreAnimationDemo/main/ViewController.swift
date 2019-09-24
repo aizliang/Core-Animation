655255e6d4590b21eef05a449ccddc4872ff3ea1
windazl <749994379@qq.com>
2019-09-24 19:26:22 +0800

    Swift Demo

---
+++ CoreAnimationDemo/CoreAnimationDemo/main/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  CoreAnimationDemo
+//
+//  Created by ai on 2019/9/24.
+//  Copyright © 2019 wind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController  {
+
+    let moduleTitles = ["图层树", "寄宿图", "图层几何学", "视觉效果", "变化", "专用图层", "隐式动画", "显示动画",
+                        "动画时间", "缓冲", "基于定时器的动画", "性能调优", "高效绘图", "图像IO", "图层性能"]
+    let moduleVCNames: [Any] = ["图层树", "寄宿图", "图层几何学", "视觉效果", "变化", "专用图层", ImplicitAnimationVC(), "显示动画",
+                         "动画时间", "缓冲", "基于定时器的动画", "性能调优", "高效绘图", "图像IO", "图层性能"]
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Core Animation"
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return moduleTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell.init(style: .value1, reuseIdentifier: "cell")
+        
+        guard indexPath.row < moduleTitles.count else {
+            return cell
+        }
+        
+        cell.textLabel?.text = moduleTitles[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard indexPath.row < moduleVCNames.count else {
+            return
+        }
+        
+        let moduleVC = moduleVCNames[indexPath.row]
+        if let detaulVC = moduleVC as? UIViewController {
+            self.navigationController?.pushViewController(detaulVC , animated: true)
+        }
+    }
+}
+
